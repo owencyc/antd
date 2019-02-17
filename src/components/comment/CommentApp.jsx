f64360aa9404187e861eb2870b794c762b1d6694
“owenc <owencyc@gmail.com>
2019-02-17 14:04:12 +0800

    api

--- src/components/comment/CommentApp.jsx
+++ src/components/comment/CommentApp.jsx
@@ -14,13 +14,21 @@ class CommentApp extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            c_list: [{
-                author: 'owenc',
-                content: '沙发！！！！！！！'
-            }],
+            c_list: [],
             content: '',
             submitting: false
         };
+        fetch(
+            'https://www.easy-mock.com/mock/5c68f1f1160b1240c2965a90/api/allcomments'
+        )
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                this.setState({c_list:data.data});
+
+            })
+            .catch(e => console.log('错误:', e))
+
     }
 
     handleChange(e) {
